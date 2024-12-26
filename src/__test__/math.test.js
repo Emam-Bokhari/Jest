bb03c017986fc3d118c0597e8c00bd14f986c49f
Moshfiqur Rahman <studentemam@gmail.com>
2024-12-26 23:12:18 +0600

    test: add test cases for multiply function

--- src/__test__/math.test.js
+++ src/__test__/math.test.js
@@ -1,4 +1,4 @@
-const { addition, substraction } = require("../commonMatchers/math");
+const { addition, substraction, multiply } = require("../commonMatchers/math");
 
 describe("Basic mathmetics test ", () => {
   it("should addition two numbers", () => {
@@ -27,5 +27,21 @@ describe("Basic mathmetics test ", () => {
 
   it("should subtract two negative numbers", () => {
     expect(substraction(-10, -4)).toBe(-6);
+  });
+
+  it("should multply when two positive numbers", () => {
+    expect(multiply(5, 10)).toBe(50);
+  });
+
+  it("should multiply when a number is negative", () => {
+    expect(multiply(5, -2)).toBe(-10);
+  });
+
+  it("should multiply when two negative numbers", () => {
+    expect(multiply(-5, -5)).toBe(25);
+  });
+
+  it("should multiply when two numbers is zero", () => {
+    expect(multiply(0, 0)).toBe(0);
   });
 });
